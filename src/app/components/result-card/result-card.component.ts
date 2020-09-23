443b004201bd35a6517ca5a9232a8edd91a33934
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-09-23 21:41:35 +0200

    Move undefined to the edges

--- src/app/components/result-card/result-card.component.ts
+++ src/app/components/result-card/result-card.component.ts
@@ -1,44 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Weather } from 'src/app/services/weather.service';
-
-const formatTime = (time: number | undefined): string =>
-  time !== undefined
-    ? new Date(time * 1000).toLocaleTimeString([], {
-        hour: '2-digit',
-        minute: '2-digit',
-      })
-    : '';
 
 @Component({
   selector: 'app-result-card',
   templateUrl: './result-card.component.html',
   styleUrls: ['./result-card.component.scss'],
 })
-export class ResultCardComponent implements OnInit {
-  constructor() {}
-
-  @Input() set result(result: Weather | undefined) {
-    this.currentResult = result;
-    this.iconCode = result?.weather[0].id ?? 0;
-    this.sunriseDate = formatTime(result?.sys?.sunrise);
-
-    const sunset = result?.sys?.sunset;
-    this.sunsetDate = formatTime(sunset);
-
-    const currentTime = Date.now() / 1000;
-    this.isAfterSunset = sunset !== undefined ? currentTime > sunset : false;
+export class ResultCardComponent {
+  @Input() set result(result: Weather) {
+    this.result_ = result;
+    this.iconCode = result.type === 800 ? 9 : Math.floor(result.type / 100);
+    this.isAfterSunset = Date.now() > result.sunsetAt.getTime();
   }
-  get result(): Weather | undefined {
-    return this.currentResult;
+  get result(): Weather {
+    return this.result_;
   }
 
   @Input() isResultNotFound!: boolean;
 
-  private currentResult: Weather | undefined = undefined;
+  private result_!: Weather;
   iconCode = 0;
-  sunsetDate = '';
-  sunriseDate = '';
   isAfterSunset = false;
-
-  ngOnInit(): void {}
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -4,7 +4,7 @@ import { catchError, retry, map } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { environment } from '../../environments/environment';
 
-export type Weather = {
+type WeatherResponse = {
   name: string;
   weather: {
     description: string;
@@ -17,6 +17,33 @@ export type Weather = {
   sys: {
     sunrise: number;
     sunset: number;
+  };
+};
+
+export type Weather = {
+  city: string;
+  description: string;
+  type: number;
+  temp: number;
+  tempFeelsLike: number;
+  sunriseAt: Date;
+  sunsetAt: Date;
+};
+
+const toWeather = (res: WeatherResponse): Weather => {
+  const firstWeather = res.weather[0] ?? {
+    description: 'no description available',
+    id: 0,
+  };
+
+  return {
+    city: res.name,
+    description: firstWeather.description,
+    type: firstWeather.id,
+    temp: res.main.temp,
+    tempFeelsLike: res.main.feels_like,
+    sunriseAt: new Date(res.sys.sunrise * 1000),
+    sunsetAt: new Date(res.sys.sunset * 1000),
   };
 };
 
@@ -34,18 +61,24 @@ export class WeatherService {
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.apikey}&units=metric`
       )
       .pipe(
-        map((res) => res as Weather),
+        map((res) => res as WeatherResponse),
         retry(3),
+        map(toWeather),
         catchError(this.handleError)
       );
   }
 
-  handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      console.log(error);
+  handleError(error: HttpErrorResponse | Error) {
+    if (error instanceof HttpErrorResponse) {
+      if (error.error instanceof Error) {
+        return throwError(error.error.message);
+      } else {
+        return throwError(error.status + ': ' + error.message);
+      }
+    } else if (error instanceof Error) {
+      return throwError(error.message);
     } else {
-      console.log(error.status);
+      return throwError('something bad happend');
     }
-    return throwError('error');
   }
 }
