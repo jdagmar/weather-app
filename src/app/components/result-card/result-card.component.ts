0834a0a68d716566c1d340381fe7ddbe87d310dd
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-09-24 22:40:27 +0200

    Use new icon component

--- src/app/components/result-card/result-card.component.ts
+++ src/app/components/result-card/result-card.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { Weather } from 'src/app/services/weather.service';
 
+const setIconName = (name: string, isAfterSunset: Boolean) =>
+  isAfterSunset ? `${name}-moon` : `${name}-sun`;
+
 @Component({
   selector: 'app-result-card',
   templateUrl: './result-card.component.html',
@@ -9,8 +12,32 @@ import { Weather } from 'src/app/services/weather.service';
 export class ResultCardComponent {
   @Input() set result(result: Weather) {
     this.result_ = result;
-    this.iconCode = result.type === 800 ? 9 : Math.floor(result.type / 100);
-    this.isAfterSunset = Date.now() > result.sunsetAt.getTime();
+    const iconCode = result.type === 800 ? 9 : Math.floor(result.type / 100);
+    const isAfterSunset = Date.now() > result.sunsetAt.getTime();
+
+    switch (iconCode) {
+      case 2:
+        this.iconName = setIconName('thunderstorm', isAfterSunset);
+        break;
+      case 3:
+        this.iconName = setIconName('drizzle', isAfterSunset);
+        break;
+      case 5:
+        this.iconName = setIconName('rain', isAfterSunset);
+        break;
+      case 6:
+        this.iconName = setIconName('snow', isAfterSunset);
+        break;
+      case 7:
+        this.iconName = setIconName('atmosphere', isAfterSunset);
+        break;
+      case 8:
+        this.iconName = setIconName('clouds', isAfterSunset);
+        break;
+      default:
+        this.iconName = setIconName('clear', isAfterSunset);
+        break;
+    }
   }
   get result(): Weather {
     return this.result_;
@@ -19,6 +46,5 @@ export class ResultCardComponent {
   @Input() isResultNotFound!: boolean;
 
   private result_!: Weather;
-  iconCode = 0;
-  isAfterSunset = false;
+  iconName = '';
 }
