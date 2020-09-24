6a3b6f1d386e1196b75b2b5e7fb7b5356bcb31f7
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-09-24 22:46:11 +0200

    Remove unused component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CitiesListComponent } from './components/cities-list/cities-list.component';
 import { SearchFormComponent } from './components/search-form/search-form.component';
 import { ResultCardComponent } from './components/result-card/result-card.component';
 import { RoundPipe } from './pipes/round.pipe';
@@ -14,7 +13,6 @@ import { IconComponent } from './components/icon/icon.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CitiesListComponent,
     SearchFormComponent,
     ResultCardComponent,
     RoundPipe,
