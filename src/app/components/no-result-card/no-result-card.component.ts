af8051f5c89c91f63a49cce069cb0142641da59f
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-10-11 22:13:21 +0200

    Make no result card dumb

--- src/app/components/no-result-card/no-result-card.component.ts
+++ src/app/components/no-result-card/no-result-card.component.ts
@@ -1,14 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-no-result-card',
   templateUrl: './no-result-card.component.html',
   styleUrls: ['./no-result-card.component.scss'],
 })
-export class NoResultCardComponent implements OnInit {
-  constructor() {}
-
+export class NoResultCardComponent {
   @Input() city!: string;
-
-  ngOnInit(): void {}
 }
