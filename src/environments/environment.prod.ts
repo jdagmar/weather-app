03f7494a4b8b84a3a4485f855efab38bc5ccf4d5
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2021-03-02 22:45:20 +0100

    Adjust colors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true,
-  openWeatherMap: {
-    apiKey: undefined,
-  },
+   production: true,
+   openWeatherMap : {
+        apiKey: 'undefined'
+    },
 };
